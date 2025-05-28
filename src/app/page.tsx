ef5ccd39cecc6f03c9388e166bf6d156afe86773
seonysun <csh99240@naver.com>
2025-05-28 01:27:54 +0000

    Deploy from GitHub Actions

--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div>프리텐다드</div>;
-}
--- src/providers/type.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-
-type ProviderProps = {
-  children: ReactNode;
-};
-
-export default ProviderProps;
